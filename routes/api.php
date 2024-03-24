43ced10f9ce7474e1c66c04628c28a5bbba0890a
Hamza Alkoozy <hamza.alkoozy@outlook.com>
2024-03-24 15:08:20 +0400

    fixes

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// TODO -hamza undo comment
 Route::middleware('auth:sanctum')->group( function () {
     // User API endpoints
     Route::post('/user', [UserController::class, 'store']);
